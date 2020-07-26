f9c57aeb5fb5de08758f2a7eeb7af5cf628f6336
Rebecca Taylor <bex@beccasaur.us>
2020-07-25 19:58:37 -0700

    assert/refute no longer eval

--- assert.sh
+++ assert.sh
@@ -1,8 +1,10 @@
 assert() {
-  eval "$*"
+  local command="$1"
+  shift
+  "$command" "$@"
   if [ $? -ne 0 ]
   then
-    echo "Expected to succeed, but failed: \$ $*" >&2
+    echo "Expected to succeed, but failed: \$ $command $@" >&2
     exit 1
   fi
 }
--- refute.sh
+++ refute.sh
@@ -1,8 +1,10 @@
 refute() {
-  eval "$*"
+  local command="$1"
+  shift
+  "$command" "$@"
   if [ $? -eq 0 ]
   then
-    echo "Expected to fail, but succeeded: \$ $*" >&2
+    echo "Expected to fail, but succeeded: \$ $command $@" >&2
     exit 1
   fi
 }
